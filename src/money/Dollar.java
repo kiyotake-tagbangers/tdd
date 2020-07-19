1ffe1f69ca6aa949045e27963784d425bc73cb32
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-07-19 10:54:57 +0900

    resolve(object equality)

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -1,11 +1,13 @@
 package money;
 
+import java.util.Objects;
+
 /**
  * @author KIYOTA, Takeshi
  */
 public class Dollar {
 
-    public int amount;
+    int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
@@ -13,5 +15,11 @@ public class Dollar {
 
     public Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
     }
 }
