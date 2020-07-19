8192423b730c9ff9cd954f2c990c445c00a40054
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-07-19 12:03:49 +0900

    delete franc equal method

--- src/money/Franc.java
+++ src/money/Franc.java
@@ -5,8 +5,8 @@ import java.util.Objects;
 /**
  * @author KIYOTA, Takeshi
  */
-public class Franc {
-    private int amount;
+public class Franc extends Money{
+
     Franc(int amount){
         this.amount = amount;
     }
@@ -14,11 +14,5 @@ public class Franc {
 
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        Franc franc = (Franc) o;
-        return amount == franc.amount;
     }
 }
