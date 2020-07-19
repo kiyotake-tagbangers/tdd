6a9a4321701e8eaaba940c96fad79cd8536a1fb7
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-07-19 16:13:05 +0900

    move times method to Money class

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -10,8 +10,4 @@ class Dollar extends Money {
     public Dollar(int amount, String currency) {
         super(amount, currency);
     }
-
-    Money times(int multiplier) {
-        return new Money(amount * multiplier, currency);
-    }
 }
