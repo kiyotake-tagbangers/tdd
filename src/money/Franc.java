5e9cc5477f320e164093dceaa88523eeaa813f0f
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-07-19 16:13:05 +0900

    move times method to Money class

--- src/money/Franc.java
+++ src/money/Franc.java
@@ -5,13 +5,9 @@ import java.util.Objects;
 /**
  * @author KIYOTA, Takeshi
  */
-class Franc extends Money{
+class Franc extends Money {
 
     public Franc(int amount, String currency) {
         super(amount, currency);
-    }
-
-    Money times(int multiplier) {
-        return new Money(amount * multiplier, currency);
     }
 }
